64ddd2dfd0b5d23495b1c468bfa31b77dea4643e
anuragtiwari1 <anuragkumartiwari79@gmail.com>
2020-07-31 02:19:54 +0530

    added basic auth guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,20 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AuthGuard } from './auth-guard.guard';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, DashboardComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
-      { path: '', component: DashboardComponent },
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+        canActivate: [AuthGuard],
+      },
       { path: 'login', component: LoginComponent },
+      { path: '', component: LoginComponent },
     ]),
   ],
   providers: [],
