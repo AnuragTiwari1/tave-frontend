2de8ecbce50754c4afc876f99604c64d7aeb555f
anuragtiwari1 <anuragkumartiwari79@gmail.com>
2020-08-01 19:09:49 +0530

    added fontawsome 5 angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { CreateAccountComponent } from './pages/login/create-account/create-acco
 import { ForgotPasswordComponent } from './pages/login/forgot-password/forgot-password.component';
 import { LoginComponentComponent } from './pages/login/login-component/login-component.component';
 import { SetPasswordComponent } from './pages/login/set-password/set-password.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -31,6 +33,8 @@ import { SetPasswordComponent } from './pages/login/set-password/set-password.co
       { path: 'login', component: LoginComponent },
       { path: '', component: LoginComponent },
     ]),
+    ReactiveFormsModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/pages/login/create-account/create-account.component.ts
+++ src/app/pages/login/create-account/create-account.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { faFilm } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-create-account',
   templateUrl: './create-account.component.html',
-  styleUrls: ['./create-account.component.css']
+  styleUrls: ['./create-account.component.css'],
 })
 export class CreateAccountComponent implements OnInit {
+  filmIcon = faFilm;
 
-  constructor() { }
+  createAccountForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+  });
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
