dae2b1adb171c0a1591319db85e15a5fe3d05342
anuragtiwari1 <anuragkumartiwari79@gmail.com>
2020-08-28 16:54:07 +0530

    added leads create form

--- src/app/pages/jobs/components/jobs-create/jobs-create.component.ts
+++ src/app/pages/jobs/components/jobs-create/jobs-create.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-jobs-create',
   templateUrl: './jobs-create.component.html',
-  styleUrls: ['./jobs-create.component.css']
+  styleUrls: ['./jobs-create.component.scss'],
 })
 export class JobsCreateComponent implements OnInit {
+  formSumitAttempt: boolean;
 
-  constructor() { }
+  jobsForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    brand: new FormControl('', [Validators.required]),
+    jobType: new FormControl('', [Validators.required]),
+    jobDate: new FormControl('', [Validators.required]),
 
-  ngOnInit(): void {
+    rating: new FormControl(3, [Validators.required]),
+    confidence: new FormControl('Medium', [Validators.required]),
+    decideBy: new FormControl('', [Validators.required]),
+    maxBudget: new FormControl('', [Validators.required]),
+    source: new FormControl('', [Validators.required]),
+
+    leadNote: new FormControl('', [Validators.required]),
+
+    stage: new FormControl('Lead', [Validators.required]),
+    leadInquiredOn: new FormControl('', [Validators.required]),
+    bookedOn: new FormControl('', [Validators.required]),
+  });
+
+  constructor(private toastr: ToastrService) {}
+
+  ngOnInit(): void {}
+
+  isFieldValid(field: string): boolean {
+    return (
+      (!this.jobsForm.get(field).valid && this.jobsForm.get(field).touched) ||
+      (this.formSumitAttempt && !this.jobsForm.get(field).valid)
+    );
   }
+  onSubmit() {
+    this.formSumitAttempt = true;
+    const val = this.jobsForm.value;
 
+    if (this.jobsForm.valid) {
+    } else {
+      this.toastr.error(
+        'Please fill all the fields before submitting',
+        'Form has errored'
+      );
+    }
+  }
 }
