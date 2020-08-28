d2c9fb54e81c2cbeb4f081eb735b962b2371b2be
anuragtiwari1 <anuragkumartiwari79@gmail.com>
2020-08-29 04:19:52 +0530

    added notes tab

--- src/app/pages/leads/components/lead-notes/lead-notes.component.ts
+++ src/app/pages/leads/components/lead-notes/lead-notes.component.ts
@@ -1,15 +1,65 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { LeadsService } from 'src/app/services/leads.service';
 
 @Component({
   selector: 'app-lead-notes',
   templateUrl: './lead-notes.component.html',
-  styleUrls: ['./lead-notes.component.css']
+  styleUrls: ['./lead-notes.component.scss'],
 })
 export class LeadNotesComponent implements OnInit {
+  notesList = [{}, {}, {}, {}];
+  closeResult = '';
+  noteFormSubmitAttempt: boolean;
 
-  constructor() { }
+  notesForm = new FormGroup({
+    note: new FormControl('', [Validators.required]),
+    type: new FormControl('', [Validators.required]),
+  });
 
-  ngOnInit(): void {
+  constructor(
+    public leadServices: LeadsService,
+    private modalService: NgbModal
+  ) {}
+
+  ngOnInit(): void {}
+
+  open(content) {
+    this.modalService
+      .open(content, { ariaLabelledBy: 'modal-basic-title' })
+      .result.then(
+        (result) => {
+          this.closeResult = `Closed with: ${result}`;
+        },
+        (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        }
+      );
   }
 
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+  onNoteSubmit() {
+    this.noteFormSubmitAttempt = true;
+
+    const val = this.notesForm.value;
+
+    if (this.notesForm.valid) {
+      this.leadServices
+        .addNote(this.leadServices.currentId, val)
+        .subscribe(() => {
+          this.modalService.dismissAll();
+          this.noteFormSubmitAttempt = false;
+        });
+    }
+  }
 }
