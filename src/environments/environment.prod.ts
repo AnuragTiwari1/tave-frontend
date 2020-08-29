87472a58d5626fffc64b3abdd67c560f0a049cf6
anuragtiwari1 <anuragkumartiwari79@gmail.com>
2020-08-29 18:08:52 +0530

    deploy to server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // baseUrl: 'https://sabbmanglam.com/tave/index.php',
-  baseUrl: 'https://d03107ccf211.ngrok.io/ProjectCode/tave/index.phpp',
+  baseUrl: 'https://d03107ccf211.ngrok.io/ProjectCode/tave/index.php',
 };
