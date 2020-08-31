61f5bda3056b1ab69ced7e2fead2c1ede7136673
anuragtiwari1 <anuragkumartiwari79@gmail.com>
2020-08-31 19:37:22 +0530

    hot fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // baseUrl: 'https://sabbmanglam.com/tave/index.php',
-  baseUrl: 'http://cd19320f0597.ngrok.io/ProjectCode/tave/index.php',
+  baseUrl: 'https://cd19320f0597.ngrok.io/ProjectCode/tave/index.php',
 };
